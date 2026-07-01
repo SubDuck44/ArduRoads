23c1621546ec26e599557816fd6371732aecf179
Melinda <melinda.stobbe@mail.de>
2026-07-01 11:53:53 +0200

    simplify flake

--- flake.nix
+++ flake.nix
@@ -1,41 +1,12 @@
 {
   outputs = { flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs.lib.fileset) toSource unions;
-        stdenv = pkgs.gccStdenv; # pkgs.clangStdenv;
-      in
+      let pkgs = import nixpkgs { inherit system; }; in
       rec {
-        packages.default = stdenv.mkDerivation {
-          pname = "arduino";
-          version = "0.0.1";
-
-          src = toSource {
-            root = ./.;
-            fileset = unions [
-              ./src
-            ];
-          };
-
-          nativeBuildInputs = with pkgs; [
-            arduino-cli
-          ];
-
-          buildInputs = with pkgs; [
-          ];
-        };
-
-        devShells.default = (pkgs.mkShell.override {
-          inherit stdenv;
-        }) {
-          inputsFrom = [ packages.default ];
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            arduino-language-server
+            arduino-cli
             avrdude
-            clang-tools
-            just
-            minicom
             usbutils
           ];
         };
